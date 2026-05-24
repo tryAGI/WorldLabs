a72fd752d3db61f3f4ca2c88601f59ba8563b350
Konstantin S. <havendv@gmail.com>
2026-05-24 10:33:30 +0400

    feat: Updated OpenAPI spec (#14)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/WorldLabs/Generated/WorldLabs.WorldLabsClient.HealthzHealthzGet.g.cs
+++ src/libs/WorldLabs/Generated/WorldLabs.WorldLabsClient.HealthzHealthzGet.g.cs
@@ -347,17 +347,15 @@ namespace WorldLabs
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::WorldLabs.ApiException(
+                                    throw global::WorldLabs.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -392,17 +390,15 @@ namespace WorldLabs
                                     {
                                     }
 
-                                    throw new global::WorldLabs.ApiException(
+                                    throw global::WorldLabs.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
